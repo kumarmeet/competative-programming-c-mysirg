e18ca4e15daeff799d71a9a6026076216b36d8c1
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-03-18 16:42:19 +0530

    Competative programming v1

---
+++ Competative_Programming_C_MySirg/main.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //call below function here and execute that
+    return 0;
+}
+
+void print_prime_factorization()
+{
+    int n, i = 2;
+
+    scanf("%d", &n);
+
+    while(i <= n)
+    {
+        if(n % i == 0)
+        {
+            printf("%d ", i);
+            n = n / i;
+            i = 2;
+        }
+        else
+            i++;
+    }
+    return;
+}
+
+void print_factors()
+{
+    int n1, i = 1;
+    scanf("%d", &n1);
+
+    while(i <= n1)
+    {
+        if(n1 % i == 0)
+        {
+            printf("%d ",i);
+        }
+        i++;
+    }
+    return;
+}
+
+void lcm_two_num()
+{
+    int n1, n2, val, i;
+    scanf("%d %d", &n1, &n2);
+
+    val = (n1 > n2) ? n1 : n2;
+
+    for(i = 1; i <= val; i++)
+    {
+        if(val % n1 == 0 && val % n2 == 0)
+        {
+            printf("%d ", val);
+            break;
+        }
+
+        ++val;
+    }
+    return;
+}
+
+void print_n_prime()
+{
+    int i = 1, j, count = 0, n, l;
+    scanf("%d", &n);
+
+    for(l = 1; l <= n; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            if(i % j == 0)
+            {
+                count++;
+            }
+        }
+        if(count == 2)
+        {
+            printf("%d ", i);
+            l++;
+        }
+        count = 0;
+    }
+    return;
+}
+
+void count_currency_notes_num()
+{
+    int n, t, fh, h, ft, tw, tn;
+
+    if(n < 10)
+        printf("Enter valid amount");
+
+    else
+    {
+        scanf("%d", &n);
+
+        t = n / 1000;
+        n = n % 1000;
+
+        fh = n / 500;
+        n = n % 500;
+
+        h = n / 100;
+        n = n % 100;
+
+        ft = n / 50;
+        n = n % 50;
+
+        tw = n / 20;
+        n = n % 20;
+
+        tn = n / 10;
+
+        printf("1000 %d\n", t);
+        printf("500 %d\n", fh);
+        printf("100 %d\n", h);
+        printf("50 %d\n", ft);
+        printf("20 %d\n", tw);
+        printf("10 %d\n", tn);
+    }
+    return;
+}
+
+void find_attendance_percentage()
+{
+    int daysOfAttendance, jul, aug, sep, oct, nov;
+    float attendacePercentage;
+
+    scanf("%d %d %d %d %d", &jul, &aug, &sep, &oct, &nov);
+
+    if(jul >= 0 && jul <= 31 &&
+            aug >= 0 && aug <= 31 &&
+            sep >= 0 && sep <= 31 &&
+            oct >= 0 && oct <= 31 &&
+            nov >= 0 && nov <= 31)
+    {
+        daysOfAttendance = jul + aug + sep + oct + nov;
+        attendacePercentage = ((float) daysOfAttendance / 153) * 100;
+        printf("%.2f", attendacePercentage);
+    }
+    return;
+}
+
+void diff_n_and_rev()
+{
+    int n, t, diff, r, revNum = 0;
+
+    scanf("%d", &n);
+    t = n;
+    while(n != 0)
+    {
+        r = n % 10;
+        revNum = revNum * 10 + r;
+        n = n / 10;
+    }
+    diff = t - revNum;
+
+    printf("%d", diff);
+    return;
+}
+
+void perfect_number()
+{
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    for(i = 1; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    if(n == sum)
+        printf("%d ", 1);
+    else
+    {
+        printf("%d ", -1);
+    }
+    return;
+}
+
+void prime_number_decresing_order()
+{
+    int n1, n2, i, j, count = 0, t;
+
+    scanf("%d %d", &n1, &n2);
+
+    if(n1 > n2)
+    {
+        t = n2;
+        n2 = n1;
+        n1 = t;
+    }
+
+    for(i = n2; i >= n1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            if(i % j == 0)
+            {
+                count++;
+            }
+        }
+
+        if(count == 2)
+        {
+            printf("%d ", i);
+        }
+        count = 0;
+    }
+    return;
+}
