e2f61a85733fd613f40975117d7a34a554579155
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-03-30 16:46:41 +0530

    add a funciton
    convert digits to number name

--- Competative_Programming_C_MySirg/main.c
+++ Competative_Programming_C_MySirg/main.c
@@ -7,6 +7,67 @@ int main()
     return 0;
 }
 
+void convert_digits_to_number_name()
+{
+        int n, temp = 0, rev = 0;
+
+    scanf("%d", &n);
+    if(n >= 10000)
+    {
+        do
+        {
+            temp = n % 10;
+            n /= 10;
+            rev = rev * 10 + temp;
+        }
+        while(n);
+
+        temp = 0;
+        n = 0;
+
+
+        do
+        {
+            temp = rev % 10;
+            rev /= 10;
+
+            switch(temp)
+            {
+            case 0:
+                printf("Zero ");
+                break;
+            case 1:
+                printf("One ");
+                break;
+            case 2:
+                printf("Two ");
+                break;
+            case 3:
+                printf("Three ");
+                break;
+            case 4:
+                printf("Four ");
+                break;
+            case 5:
+                printf("Five ");
+                break;
+            case 6:
+                printf("Six ");
+                break;
+            case 7:
+                printf("Seven ");
+                break;
+            case 8:
+                printf("Eight ");
+                break;
+            case 9:
+                printf("Nine ");
+                break;
+            }
+        }
+        while(rev);
+    }
+}
 void print_prime_factorization()
 {
     int n, i = 2;
