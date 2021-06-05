723a62d33250b1a7a011cbcd7fcf7e9862b5becd
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-06-05 16:24:18 +0530

    Added void print_n_traid_numbers(int n);
    This function prints n times traid number

--- Competative_Programming_C_MySirg/main.c
+++ Competative_Programming_C_MySirg/main.c
@@ -14,6 +14,58 @@ int main()
     return 0;*/
 }
 
+void print_n_traid_numbers(int n)
+{
+    int d1, d2, d3, tmp, a, b, c;
+
+    // generate three digit numbers
+    for (int num = 100; num <= 333; num++)
+    {
+        // check for unique digits in the first number
+        for (int i = num; i <= num * 3; i += num)
+        {
+            tmp = i;
+            d1 = tmp % 10;
+
+            tmp = tmp / 10;
+            d2 = tmp % 10;
+
+            tmp = tmp / 10;
+            d3 = tmp % 10;
+
+            if (d1 == d2 || d2 == d3 || d3 == d1)
+                goto next;
+        }
+
+        // check whether the given three numbers have unique digits or not
+        for (int a = num; a > 0; a /= 10)
+        {
+            d1 = a % 10;
+            for (int b = num * 2; b > 0; b /= 10)
+            {
+                d2 = b % 10;
+                for (int c = num * 3; c > 0; c /= 10)
+                {
+                    d3 = c % 10;
+
+                    if (d1 == d2 || d2 == d3 || d3 == d1)
+                        goto next;
+                }
+            }
+        }
+        a = num;
+        b = num * 2;
+        c = num * 3;
+        while(n)
+        {
+            printf("%d %d %d\n", a, b, c);
+            n--;
+            break;
+        }
+    next:;
+    }
+}
+
 int createWord(char word[][40],char *str, int n, int size)
 {
     int i, j, count = 0, k = 0;
